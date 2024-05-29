64856969e03a77446786972f70770882c16234ee
Octarian2003 <octarianrh@gmail.com>
2024-05-29 08:43:47 +0700

    Notes

---
+++ notes/lib/widgets/note_dialog.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:notes/models/note.dart';
+import 'package:notes/services/note_service.dart';
+
+class NoteDialog extends StatefulWidget {
+  final Note? note;
+
+  const NoteDialog({super.key, this.note});
+
+  @override
+  State<NoteDialog> createState() => _NoteDialogState();
+}
+
+class _NoteDialogState extends State<NoteDialog> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  XFile? _imageFile;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.note != null) {
+      _titleController.text = widget.note!.title;
+      _descriptionController.text = widget.note!.description;
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = pickedFile;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(widget.note == null ? 'Add Notes' : 'Update Notes'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Title', textAlign: TextAlign.start),
+          TextField(controller: _titleController),
+          const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text('Description: '),
+          ),
+          TextField(controller: _descriptionController, maxLines: null),
+          const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text('Image: '),
+          ),
+          Expanded(child: _imageFile != null
+              ? Image.network(_imageFile!.path, fit: BoxFit.fitHeight)
+              : (widget.note?.imageUrl != null && Uri.parse(widget.note!.imageUrl!).isAbsolute
+                  ? Image.network(widget.note!.imageUrl!, height: 50,)
+                  : Container())),
+          
+          TextButton(onPressed: _pickImage, child: const Text('Pick Image')),
+        ],
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            String? imageUrl;
+            if (_imageFile != null) {
+              imageUrl = await NoteService.uploadImage(_imageFile!);
+            } else {
+              imageUrl = widget.note?.imageUrl;
+            }
+            Note note = Note(
+              id: widget.note?.id,
+              title: _titleController.text,
+              description: _descriptionController.text,
+              imageUrl: imageUrl,
+              createdAt: widget.note?.createdAt,
+            );
+
+            if (widget.note == null) {
+              await NoteService.addNote(note);
+            } else {
+              await NoteService.updateNote(note);
+            }
+            Navigator.of(context).pop();
+          },
+          child: Text(widget.note == null ? 'Add' : 'Update'),
+        ),
+      ],
+    );
+  }
+}
